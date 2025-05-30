3b1d2c5cf7bd6d1abf368007d7105d4b96ed4bce
Piyush Shivnani <114189599+Piyushh10@users.noreply.github.com>
2025-05-30 10:57:43 +0530

    Time: 5 ms (70%), Space: 44.1 MB (49.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String numStr = String.valueOf(x);
-        StringBuilder sb = new StringBuilder(numStr);
-        return numStr.equals(sb.reverse().toString());
+        int n = x;
+        int nn = 0;
+        if(x<0){
+            return false;
+        }
+        while(n!=0){
+            nn = nn*10 + (n%10);
+            n = n/10;
+        }
+        if(nn==x){
+            return true;
+        }
+        return false;
     }
 }
