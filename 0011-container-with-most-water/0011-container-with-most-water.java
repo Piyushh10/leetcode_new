176145b68b39453ed26a825a82396970cc56c892
Piyush Shivnani <114189599+Piyushh10@users.noreply.github.com>
2025-09-12 19:44:48 +0530

    Time: 5 ms (76.66%), Space: 57.4 MB (86.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,48 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        int start = 0;
-        int end = height.length-1;
-        int max = 0;
-
-        while(start < end){
-            int l = Math.min(height[start], height[end]);
-            int b = end - start;
-            int area = l*b;
-            max = Math.max(max, area);
-
-            if(height[end] > height[start]){
-                start++;
+        int ans = 0;
+        int l = 0;
+        int r = height.length-1;
+        while(l<r){
+            int area = (r-l) * Math.min(height[l], height[r]);
+            ans = Math.max(ans, area);
+            if(height[l] <= height[r]){
+                l++;
             }else{
-                end--;
+                r--;
             }
         }
-        return max;
+        return ans;
     }
 }
-
-/*
-
-class Solution {
-    public int maxArea(int[] height) {
-        int n = height.length;
-        int startB = 0;
-        int endB = 0;
-        int startL = 0;
-        int endL = 0;
-        int area = 0;
-        int max = 0;
-        for(int i = 0; i<n-1; i++){
-            for(int j = i+1;j<n;j++){
-                startB = i;
-                endB = j;
-                startL = height[i];
-                endL = height[j];
-                area = (endB-startB)*(Math.min(startL, endL));
-                max = Math.max(max, area);
-            }
-        }
-        return max;
-    }
-}
-
-*/
